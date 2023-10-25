81a2126ab7ff2b10690dd11f8a15c08d6b450bb1
Chrystian Chwaja <chrystian.chwaja@microsoft.wsei.edu.pl>
2023-10-25 22:51:08 +0200

    maquette fetch data

--- chrystian-chwaja/src/components/Content.tsx
+++ chrystian-chwaja/src/components/Content.tsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function Content() {
+type AlbumRequest = {
+    albumId: number,
+    id: number,
+    title: string,
+    url: string,
+    thumNailUrl?: string,
+}
+// type PostRequest = {
+//     userId: number,
+//     id: number,
+//     title: string,
+//     body: string,
+// }
+
+function Content(props: any) {
+
+    const [data, setData] = useState<AlbumRequest>(null!);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then(res => res.json())
+                .then(data => setData(data))
+                    .finally(() => setLoading(false));
+    }, [])
+
     return (<div className="border">
         <section className="content">
-            <img className="content--img">Image</img>
+            {loading ? <span>Loading...</span> : <img src={data.url} className="content--img" alt="123"></img>}
             <div className="content--text">
                 <div className="content--local">
                     <i className="fa fa-map-marker"></i>
                     <span>Location</span>
-                    <a href="#">View on Google maps</a>
+                    <a href="https://google.com">View on Google maps</a>
                 </div>
                 <h1 className="content--title">Title</h1>
                 <div className="content--description">
